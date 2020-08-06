e7b0c6f483cba6c89e8e44262c7648ed4b627180
Karthik G G <61579922+devgg-source@users.noreply.github.com>
2020-08-06 14:24:17 +0530

    Update stack.js

--- DSandAlgo/stack.js
+++ DSandAlgo/stack.js
@@ -1,17 +1,19 @@
 /*  Stack!.. */
 
-var Stack = () => {
+class Stack {
+ constructor(){    
   this.count = 0;
   this.storage = {};
+ }
 
   //Adds the value onto the end of the stack
-  this.push = (val) => {
+  push = (val) => {
     this.storage[this.count] = val;
     this.count++;
   };
 
   //Removes and returns the value at the end of the stac
-  this.pop = () => {
+  pop = () => {
     if (this.count == 0) return undefined;
 
     this.count--;
@@ -19,19 +21,14 @@ var Stack = () => {
     delete this.storage[this.count];
     return result;
   };
-  //Return size of the stack
-  this.size = () => {
+  //Returns size of the stack
+  size = () => {
     return this.count;
   };
 
   //Returns the value at the end of the stack
 
-  this.peek = () => {
+  peek = () => {
     return this.storage[this.count - 1];
   };
 };
-
-var myStack = new Stack();
-myStack.push(1);
-myStack.push(2);
-myStack.peek();
