4a12b8ccc63d7a891b85c943ef3b1641186c611e
Karthik G G <61579922+devgg-source@users.noreply.github.com>
2020-10-08 08:04:09 +0530

    Add files via upload
    Redux

---
+++ index.js
@@ -0,0 +1,82 @@
+const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const FETCH_USERS_REQ = "FETCH_USERS_REQ";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const fetchUserReq = () => {
+  return {
+    type: FETCH_USERS_REQ,
+  };
+};
+
+const fetchUserSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+const fetchUserFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQ:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
+
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUserReq);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUserSuccess(users));
+      })
+      .catch((err) => {
+        dispatch(fetchUserFailure(err.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(fetchUsers);
